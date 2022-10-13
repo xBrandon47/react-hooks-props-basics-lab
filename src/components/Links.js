cb0e82fd61b034dea0adb461ae348785baf855f5
Brandon Moreno <brandonm99@me.com>
2022-10-12 21:17:05 -0600

    commit tree 3df0470f9dbc8cd42522f8b44ca8314a705d6b3b

---
+++ src/components/Links.js
@@ -0,0 +1,29 @@
+// import React from "react";
+// import user from "../data/user";
+
+// function Links(props) {
+//     return (
+//         <div>
+//             <h3>Links</h3>
+//             <a href={props.github}>{props.github}</a>
+//             <a href={props.linkedin}>{props.linkedin}</a>
+//         </div>
+//     );
+// }
+
+// export default Links;
+
+
+import React from "react";
+
+function Links(props) {
+  return (
+    <div>
+      <h3>Links</h3>
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}>{props.linkedin}</a>
+    </div>
+  );
+}
+
+export default Links;
